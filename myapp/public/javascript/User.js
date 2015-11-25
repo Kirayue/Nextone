051acb81ad3464461bc426c65ac808cea6c915a2
Cloudhsu <j02004080@gmail.com>
2015-11-25 19:07:42 +0800

    v11-25

--- myapp/public/javascript/User.js
+++ myapp/public/javascript/User.js
@@ -1,24 +1,5 @@
 /*global $ */
 $(document).ready(function() {         
-      $("#signup button[type='submit']").click(function(e){
-          var username = $('#usr').val();
-          var pw = $('#password').val();
-          var email = $('#mai').val();
-	  e.preventDefault();
-
-          $.ajax({
-              url:'/Signup' ,
-              data: 'username=' + username + '&password=' + pw +'&email=' + email,
-              type: "POST",
-              dataType: 'json',
-	      cache: false,
-
-              success:function(){
-                console.log('ok');
-          },
-     
-        });
-      });
     $(".form2").hide();
     $(".tab1").click(function(){
          $(".form2").hide();
@@ -28,4 +9,7 @@ $(document).ready(function() {
         $(".form1").hide();
         $(".form2").fadeIn();
     });
+   $.post('/user/username', function(username){
+   	console.log(username);	
+	});
 });
